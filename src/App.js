3c8921f6404d7ad05599f355e5b4a3fc1a9035d5
najim473 <najim7304@gmail.com>
2022-10-09 00:07:27 +0600

    image gallary grid

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Gallary from './Gallary';
+function App() {
+  return (
+    <div className="App">
+      <Gallary />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+
+export const image_data = [
+  {
+    id: 1,
+    img: '/images/img-01.jpg'
+  },
+  {
+    id: 2,
+    img: '/images/img-02.jpg'
+  },
+  {
+    id: 3,
+    img: '/images/img-03.jpg'
+  },
+  {
+    id: 4,
+    img: '/images/img-04.jpg'
+  },
+  {
+    id: 5,
+    img: '/images/img-05.jpg'
+
+  },
+  {
+    id: 6,
+    img: '/images/img-06.jpg'
+  },
+  {
+    id: 7,
+    img: '/images/img-07.jpg'
+  },
+  {
+    id: 8,
+    img: '/images/img-08.jpg'
+  },
+  {
+    id: 9,
+    img: '/images/img-09.jpg'
+  },
+  {
+    id: 10,
+    img: '/images/img-10.jpg'
+  },
+];
